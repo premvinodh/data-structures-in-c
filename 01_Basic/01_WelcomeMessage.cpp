460fd88caf4a5b58cd4574b10ed55628fd216b6c
premvinodh <premvinodh.pc@gmail.com>
2021-11-21 22:08:27 +0530

    Added 01_WelcomeMessage.cpp

---
+++ 01_Basic/01_WelcomeMessage.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : 01_WelcomeMessage.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Develop a program to display a welcome message.
+//============================================================================
+
+#include <stdio.h>
+int main() {
+	printf("Welcome to c, Prem Vinodh...");
+
+	return 1;
+}
