12b2e38db529a5f41bac514521ad1bae92e9b93e
premvinodh <premvinodh.pc@gmail.com>
2021-11-22 00:44:34 +0530

    Added 59_1DArrayDeleteElm.c

---
+++ 03_1DArrays/59_1DArrayDeleteElm.c
@@ -0,0 +1,54 @@
+//============================================================================
+// Name        : 59_1DArrayDeleteElm.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Develop a program to accept 'n' numbers and delete an element
+//			     'x' from it.
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a[14], n, i, j, p, flag = 0;
+
+	// Eclipse buffers the output of your program and so the order is
+	// not uniform. Using this sets the order to be as that of the written prg.
+	// or you can use fflush(stdout); after the first printf()
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	printf("Enter how many numbers...Maximum 14\n");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("Enter number");
+		scanf("%d", &a[i]);
+	}
+	printf("Enter search value");
+	scanf("%d", &p);
+	for (i = 0; i < n; i++) {
+		if (a[i] == p) {
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 0) {
+		printf("Unsuccess\n");
+		printf("Elements of the original array\n");
+	} 
+ {
+		p = i;
+		for (j = p + 1; j < n; j++) {
+			a[j - 1] = a[j];
+		}
+		n = n - 1;
+		printf("Elements of the array of deleting value at location :: %d\n", p);
+	}
+
+	for (i = 0; i < n; i++) {
+		printf("%d ", a[i]);
+	}
+
+	return 1;
+}
