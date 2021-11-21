3d24654a60f4a260bd858a871b23d2a3dd516851
premvinodh <premvinodh.pc@gmail.com>
2021-11-21 22:14:21 +0530

    Added 02_WelcomeMessage.cpp

---
+++ 01_Basic/02_WelcomeMessage.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : 02_WelcomeMessage.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Rewrite the above program to display another two lines.
+//============================================================================
+
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	printf("Welcome to c\n");
+	printf("\n");
+	printf("For\n");
+	printf("\n");
+	printf("II B.Sc. Students");
+
+	return 1;
+}
