7a73f28887af1b898a0d5df6445d7e30f685929e
premvinodh <premvinodh.pc@gmail.com>
2021-11-22 00:39:02 +0530

    Added 55_1DArraySecondLargestSmallestFromSortAsc.c

---
+++ 03_1DArrays/55_1DArraySecondLargestSmallestFromSortAsc.c
@@ -0,0 +1,40 @@
+//============================================================================
+// Name        : 55_1DArraySecondLargestSmallestFromSortAsc.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Develop a program to accept 'n' numbers and output the second
+//				 largest and second smallest (descending sorted order)
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a[10], n, i, j, temp;
+
+	// Eclipse buffers the output of your program and so the order is
+	// not uniform. Using this sets the order to be as that of the written prg.
+	// or you can use fflush(stdout); after the first printf()
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	printf("Enter how many numbers...Maximum 10\n");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("Enter number");
+		scanf("%d", &a[i]);
+	}
+	for (i = 0; i < n - 1; i++) {
+		for (j = i + 1; j < n; j++) {
+			if (a[i] > a[j]) {
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	printf("Second largest = %d Second smallest = %d", a[n-2], a[1]);
+
+	return 1;
+}
