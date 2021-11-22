8aa7d1210eb4c5a1ac8b9755487fac89ab6ee101
premvinodh <premvinodh.pc@gmail.com>
2021-11-22 11:37:13 +0530

    Added 76_2DArrayCountZerosBelowDiagonal.c

---
+++ 04_2DArrays/76_2DArrayCountZerosBelowDiagonal.c
@@ -0,0 +1,38 @@
+//============================================================================
+// Name        : 76_2DArrayCountZerosBelowDiagonal.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Develop a program to accept m*n matrix and output how many
+//				 elements below the diagonal are zero.
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a[5][5], m, n, i, j, count = 0;
+
+	// Eclipse buffers the output of your program and so the order is
+	// not uniform. Using this sets the order to be as that of the written prg.
+	// or you can use fflush(stdout); after the first printf()
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	printf("Enter how many rows...Maximum 1..5\n");
+	scanf("%d", &m);
+	printf("Enter how many columns...Maximum 1..5\n");
+	scanf("%d", &n);
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			printf("Enter element");
+			scanf("%d", &a[i][j]);
+			if (i > j && a[i][j] == 0) {
+				count += 1;
+			}
+		}
+	}
+	printf("Count of zeros below diagonal :: %d", count);
+
+	return 1;
+}
