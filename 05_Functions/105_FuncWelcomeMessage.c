30f03646f6070a456c7c39bdcbaaaf51171ab642
premvinodh <premvinodh.pc@gmail.com>
2021-11-22 16:37:25 +0530

    Added 105_FuncWelcomeMessage.c

---
+++ 05_Functions/105_FuncWelcomeMessage.c
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : 105_FuncWelcomeMessage.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Develop a function to display a welcome message.
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+void hello() {
+	printf("Hello...Welcome to functions");
+
+	return;
+}
+
+int main() {
+	hello();
+
+	return 1;
+}
