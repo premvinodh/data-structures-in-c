f2543ac5ede48716f3a29ca8e6c5ff9921d2f5d5
premvinodh <premvinodh.pc@gmail.com>
2021-11-24 01:05:46 +0530

    Added 165_PointerVariableDemo8.c

---
+++ 09_PointerVariables/165_PointerVariableDemo8.c
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : 165_PointerVariableDemo8.c
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Output the results of the following program.
+//============================================================================
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+	int a[7] = { 14, 24, -5, 8, 21, 0, 7 };
+
+	// Eclipse buffers the output of your program and so the order is
+	// not uniform. Using this sets the order to be as that of the written prg.
+	// or you can use fflush(stdout); after the first printf()
+	setvbuf(stdout, NULL, _IONBF, 0);
+
+	for (int i = 0; i < 7; i++) {
+		printf("%u %d\n", (a+i), *(a+i));
+	}
+
+	return 1;
+}
